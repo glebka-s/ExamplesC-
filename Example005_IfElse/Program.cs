1b3aa8f064bd09de2bd98578fa57f034059f6a3e
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-08 02:14:40 +0500

    Add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your name");
+string username = Console.ReadLine();
+
+if (username.ToLower() ==  "маша")
+{
+    Console.WriteLine("Hurray, it's Маша");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
