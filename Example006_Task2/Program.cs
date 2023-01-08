11c25eb334bd3e02bfffa27632d780f6d707cfb5
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-08 17:45:50 +0500

    Add Example006_Task2

---
+++ Example006_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// максимум двух чисел
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b)
+{
+    Console.Write("Numbers are equal");
+}
+else
+{
+    Console.Write("max = ");
+    if (a > b)
+    {
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write(b);
+    }
+}
