fbc78b7eb72fdb2a2c0f892f5c1f6fc0fb44b147
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-08 18:51:40 +0500

    Add Example011_Task13

---
+++ Example011_Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// третья цифра числа
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{   
+    int M = 0;
+    while (N >= 100)
+    {
+        M = N % 10;
+        N /= 10;
+    }
+    Console.Write(M);
+}
