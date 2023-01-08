d82f51492f937df17655a09f8c77e034ae698ddd
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-08 17:57:45 +0500

    Add Example007_Task4

---
+++ Example007_Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// максимальное трёх чисел
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b && a == c)
+{
+    Console.Write("Numbers are equal");
+}
+
+if (a > b && a > c)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+
+if (b > a && b > c)
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}
+
+if (c > a && c > b)
+{
+    Console.Write("max = ");
+    Console.Write(c);
+}
