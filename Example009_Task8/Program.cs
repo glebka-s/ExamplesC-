6c359843cf08ad466f447452ea147e8cfc4feb45
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-08 18:10:05 +0500

    Add Example009_Task8

---
+++ Example009_Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// все чётные числа от 1 до N
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 2)
+{
+    Console.Write("Нет таких чётных чисел");
+}
+else
+{   
+    int i = 2;
+    Console.Write(i);
+    i += 2;
+    while (i <= N)
+    {
+        Console.Write(", ");
+        Console.Write(i);
+        i += 2;
+    }
+}
