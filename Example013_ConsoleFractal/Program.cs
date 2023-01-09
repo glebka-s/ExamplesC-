7a21dbc29ac7f03ccd2de4bace7a8da941209715
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-09 23:43:44 +0500

    Add Example013_ConsoleFractal

---
+++ Example013_ConsoleFractal/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4); // отступ от левого и верхнего краёв соответственно
+//Console.WriteLine("#");
+
+int xa = 30, ya = 1,
+    xb = 1, yb = 20,
+    xc = 60, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("#");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("#");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("#");
+
+ya += 1;
+
+int x = xa, y = xb;
+
+int i = 0, count = 10000; // количество итераций
+
+while(i < count)
+{
+    int what = new Random().Next(0, 3); // [0, 3)
+
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;   
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;   
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;   
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("#");
+
+    i++;
+}
