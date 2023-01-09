75024a9656d8fa8c716b21b25298de328bfa15ed
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-09 22:50:02 +0500

    Add ArrayLibrary

---
+++ ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] CreateArray(int N)
+{
+    int[] array = new int[N];
+    int i = 0;
+    while (i < N)
+    {
+        int a = int.Parse(Console.ReadLine() ?? "");
+        array[i] = a;
+        i++;
+    }
+    return(array);
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    Console.Write(array[0]);
+    int i = 1;
+    while (i < len)
+    {
+        Console.Write($", {array[i]}");
+        i++;
+    }
+}
+
+int[] array = CreateArray(10);
+PrintArray(array);
--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Write your name");
-string username = Console.ReadLine();
+string username = Console.ReadLine() ?? "";
 
 if (username.ToLower() ==  "маша")
 {
