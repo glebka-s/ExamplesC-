40cb85fef59b29a227909ed77af945a99b3b32fa
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-10 16:07:16 +0500

    Add Var_Input

---
+++ Var_Input/Program.cs
@@ -0,0 +1,11 @@
+﻿double varA;
+
+while (true)
+{
+    Console.Write("Введите переменную А: ");
+    if (double.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+Console.WriteLine($"A = {varA}");
