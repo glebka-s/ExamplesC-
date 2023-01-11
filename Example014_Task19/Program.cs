416ca1cbda259366c8b3ce38a7e9b364608911b4
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-11 15:35:12 +0500

    Add Example014_Task19

---
+++ Example014_Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// является ли пятизначное число палиндромом
+int N;
+
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    if (int.TryParse(Console.ReadLine(), out N))
+    {
+        if (N > 9999 && N < 100000)
+        break;
+    }
+
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int a1 = N / 10000;
+int a2 = (N % 10000) / 1000;
+int a5 = N % 10;
+int a4 = (N % 100) / 10;
+
+if (a1 == a5 && a2 == a4)
+{
+    Console.Write($"{N} -> да");
+}
+else
+{
+    Console.Write($"{N} -> нет");
+}
