530d1cd311b122bd32e2a6403aa587eaf73b0e9a
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-18 18:17:30 +0300

    Add SelectionSort in ArrayLibrary

--- ArrayLibrary/Program.cs
+++ ArrayLibrary/Program.cs
@@ -1,10 +1,16 @@
-﻿int[] CreateArray(int N)
+﻿int[] CreateIntArray(int N)
 {
     int[] array = new int[N];
     int i = 0;
     while (i < N)
-    {
-        int a = int.Parse(Console.ReadLine() ?? "");
+    {   
+        int a;
+        while (true)
+        {   
+            if (int.TryParse(Console.ReadLine(), out a))
+                break;
+            Console.WriteLine("Ошибка ввода!");
+        }
         array[i] = a;
         i++;
     }
@@ -23,5 +29,46 @@ void PrintArray(int[] array)
     }
 }
 
-int[] array = CreateArray(10);
+void SelectionSortMinMax(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            } 
+                
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortMaxMin(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            } 
+                
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+int[] array = CreateIntArray(10);
+
 PrintArray(array);
