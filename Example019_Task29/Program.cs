fb40a11507ea41b644d133767330d1681e134b12
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-18 20:02:43 +0300

    Add Example019_Task29

---
+++ Example019_Task29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задаёт массив из 8 элементов и выводит их на экран.
+int[] CreateIntArray(int N)
+{
+    int[] array = new int[N];
+    int i = 0;
+    while (i < N)
+    {   
+        int a;
+        while (true)
+        {   
+            if (int.TryParse(Console.ReadLine() ?? "", out a))
+                break;
+            Console.WriteLine("Ошибка ввода!");
+        }
+        array[i] = a;
+        i++;
+    }
+    return(array);
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    Console.Write($"[{array[0]}");
+    int i = 1;
+    while (i < len)
+    {
+        Console.Write($", {array[i]}");
+        i++;
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateIntArray(8);
+PrintArray(array);
