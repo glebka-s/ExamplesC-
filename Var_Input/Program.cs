8a6c3960ba488a65c27c858c93b84879f23d9ad0
Gleb Startsev <startsev.gleb@gmail.com>
2023-01-20 15:39:26 +0300

    Update Var_Input

--- Var_Input/Program.cs
+++ Var_Input/Program.cs
@@ -9,5 +9,19 @@
     }
 }
 
+int GetIntPosNumberFromUser(string message, string ErrorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+        {
+            if (UserNumber > 0)
+                return UserNumber;
+        }
+        Console.WriteLine(ErrorMessage);
+    }
+}
+
 int varA = GetIntNumberFromUser("Введите целове число:", "Ошибка ввода!");
 Console.WriteLine($"A = {varA}");
