86f450793278daf02c8d3145d0d09f78411e89cd
Gleb Startsev <startsev.gleb@gmail.com>
2023-02-04 16:02:53 +0300

    Add Example034_Task64

---
+++ Example034_Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+int GetIntPosNumberFromUser(string message, string ErrorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+        {
+            if (UserNumber > 0)
+                return UserNumber;
+        }
+        Console.WriteLine(ErrorMessage);
+    }
+}
+
+void Numbers(int M, int N)
+{   
+    if (M < N)
+        Numbers(M + 1, N);
+    Console.Write($"{M} ");
+}
+
+Numbers(1, GetIntPosNumberFromUser("Введите N:", "Ошибка ввода!"));
