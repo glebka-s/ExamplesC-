6c0980edc8530e5d65b57a0d11cab20a38538ae8
Gleb Startsev <startsev.gleb@gmail.com>
2023-02-04 17:36:19 +0300

    Add Example036_Task68

---
+++ Example036_Task68/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetIntPosNumberFromUser(string message, string ErrorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+        {
+            if (UserNumber >= 0)
+                return UserNumber;
+        }
+        Console.WriteLine(ErrorMessage);
+    }
+}
+
+int Accerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+        if (m == 0)
+            return(Accerman(n - 1, 1));
+    return(Accerman(n - 1, Accerman (n, m - 1)));
+}
+
+
+int m = GetIntPosNumberFromUser("Введите m:", "Ошибка ввода!");
+int n = GetIntPosNumberFromUser("Введите n:", "Ошибка ввода!");
+
+Console.Write($"{Accerman(m, n)}");
+
