b6935897257e2a3d84f36fdc3a901427a492038e
Gleb Startsev <startsev.gleb@gmail.com>
2023-02-04 16:14:23 +0300

    Add Example035_Task66

---
+++ Example035_Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetIntNumberFromUser(string message, string ErrorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+            return UserNumber;
+        Console.WriteLine(ErrorMessage);
+    }
+}
+
+int summ(int M, int N)
+{
+    if (M == N)
+        return N;
+    return M + summ(M + 1, N);
+}
+
+int M = GetIntNumberFromUser("Введите M:", "Ошибка ввода!");
+int N = GetIntNumberFromUser("Введите N:", "Ошибка ввода!");
+
+Console.Write($"Сумма = {summ(M, N)}");
